902538ea6d2bba14b350a071d2bc6c6dfdabfa84
D. S <67660328+dimitri-sky@users.noreply.github.com>
2022-05-12 09:59:17 -0500

    Navigation Service Added

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+// Packages
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+
+// Pages
+import '../services/navigation_service.dart';
+
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitializationComplete;
 
@@ -18,6 +25,9 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     super.initState();
+    _setup().then(
+      (_) => widget.onInitializationComplete(),
+    );
   }
 
   @override
@@ -39,6 +49,18 @@ class _SplashPageState extends State<SplashPage> {
           ),
         ),
       ),
+    );
+  }
+
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {
+    GetIt.instance.registerSingleton<NavigationService>(
+      NavigationService(),
     );
   }
 }
